8c6a58da60650f0ad20eb245f708e0d7326567f7
blackbass64 <athibet.pi@gmail.com>
2025-08-01 17:30:05 +0700

    replace image with iframe for EighteenthSection and update styles

--- components/eighteenth-section.tsx
+++ components/eighteenth-section.tsx
@@ -2,17 +2,24 @@ import Image from "next/image";
 
 export default function EighteenthSection() {
 	return (
-		<section className="h-screen snap-start bg-[#171918] text-white flex flex-col justify-center items-center">
-			<div className="relative w-full h-full">
-				<Image
-					src="/images/page-23.png"
-					alt="Page 23 of the Mekong River interactive story"
-					fill
-					style={{ objectFit: "contain" }}
-					sizes="100vw"
-					priority
-				/>
-			</div>
+		<section className="h-screen snap-start bg-[#000000] text-white flex flex-col justify-center items-center overflow-hidden">
+				<div className="relative w-full h-auto">
+					<iframe
+						className="w-full h-full object-cover overflow-hidden rounded-md"
+						src="https://lookerstudio.google.com/embed/reporting/df08f7ad-0f4e-433d-a51a-afe248cf152f/page/j7FTF"
+						sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+						height="100%"
+						style={{ aspectRatio: "16/9" }}
+						allowFullScreen
+					/>
+					<div className="absolute bottom-0 left-0 right-0 w-full font-ibm text-normal 2xl:text-2xl text-gray-200 text-center">
+						<p className="w-full justify-center bg-black text-white py-8">
+							ฐานข้อมูล <span className="font-medium text-[#FFEB52]">‘ภาษีไปไหน’</span> ที่นำเสนอระบบข้อมูลการใช้จ่ายภาครัฐ ระบุว่าตั้งแต่ปี 2558 เป็นต้นมา
+							<br />มีการสร้างเขื่อนป้องกันตลิ่งกว่า <span className="font-medium text-[#FFEB52]">527 โครงการในพื้นที่ 92 ตำบล</span> รวมเป็นเงิน <span className="font-medium text-[#FFEB52]">40,162,071,758 บาท</span>
+							<br />โดยมีหน่วยงานที่รับผิดชอบทั้งหมดคือกรมโยธาธิการและผังเมือง กระทรวงมหาดไทย
+						</p>
+					</div>
+				</div>
 		</section>
 	);
 }
