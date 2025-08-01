ac40107d44692ef0bdfeff997ccce86007d04dfb
Andrew <aphinan_th@kkumail.com>
2025-08-02 02:38:49 +0700

    feat: Add Typewriter effect and new images to FourteenthSection component

--- components/fourteenth-section.tsx
+++ components/fourteenth-section.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
+
 export default function FourteenthSection() {
 	return (
-		<section className="h-screen snap-start bg-[#171918] bg-[url('/images/old-image-mekong.png')] bg-cover bg-center text-white flex flex-col justify-center items-center">
+		<section className="relative h-screen snap-start bg-[#171918] bg-[url('/images/old-image-mekong.png')] bg-cover bg-center text-white flex justify-start items-end text-6xl font-bold font-ibm p-16">
+			<Image
+				src="/images/old-flash.png"
+				alt="Old image of Mekong River"
+				className="absolute inset-0 object-cover opacity-50 w-full h-full z-30"
+				width={1920}
+				height={1080}
+				priority
+			/>
+			<Typewriter
+				words={["10 ปี กับ 4 หมื่นล้าน"]}
+				loop={0}
+				cursor
+				cursorStyle="|"
+				typeSpeed={70}
+				deleteSpeed={50}
+				delaySpeed={1000}
+			></Typewriter>
 		</section>
 	);
 }
